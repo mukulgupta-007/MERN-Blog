714c4837a285841a62e9c51a042625203d7e0ed6
Mukul <mukulgupta393@gmail.com>
2025-01-19 18:39:52 +0530

    Register and Login Pages Created Successfully

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import Post from './Post';
-import Header from './Header';
+// import Post from './Post';
+// import Header from './Header';  
 import { Route, Routes } from "react-router-dom";
 import Layout from './Layout';
 import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
-// import RegisterPage from './pages/RegisterPage';
-
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
@@ -14,9 +13,10 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
       </Route>
     </Routes>
   );
-}
+} 
 
 export default App;
