334473c021dd20a9fc91042763ab98424a83ea6f
Mukul <mukulgupta393@gmail.com>
2025-01-25 10:02:08 +0530

    Bcryptjs added for authentication

--- api/index.js
+++ api/index.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require("mongoose");
+const User = require('./models/User');
+const bcrypt = require('bcryptjs');
 const app = express();
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/register',(req,res)=>{
+app.post('/register', async (req,res) => {
     const {username,password} = req.body;
-    res.json({requestData:{username,password}});
-});
+    try{
+      const userDoc = await User.create({
+        username,
+        password:bcrypt.hashSync(password,salt),
+      });
+      res.json(userDoc);
+    } catch(e) {
+      console.log(e);
+      res.status(400).json(e);
+    }
+  });
 
 app.listen(4000);
+
+//mukulgupta393
+//zSpxCYEzJjA8VKO4
