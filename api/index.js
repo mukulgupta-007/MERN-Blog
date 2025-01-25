bd0c8361fb5d3f6f9e0247818cc933c59f9bfff0
Mukul <mukulgupta393@gmail.com>
2025-01-25 11:01:18 +0530

    Authentication for the login page has been added using JWT

--- api/index.js
+++ api/index.js
@@ -4,10 +4,11 @@ const mongoose = require("mongoose");
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const app = express();
+const jwt = require('jsonwebtoken');
 
 const salt = bcrypt.genSaltSync(10);
 
-app.use(cors());
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 
 app.post('/register', async (req,res) => {
@@ -23,6 +24,24 @@ app.post('/register', async (req,res) => {
       res.status(400).json(e);
     }
   });
+
+  app.post('/login', async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      // logged in
+      jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+        if (err) throw err;
+        res.cookie('token', token).json({
+          id:userDoc._id,
+          username,
+        });
+      });
+    } else {
+      res.status(400).json('wrong credentials');
+    }
+  });  
 
 app.listen(4000);
 
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,4 +1,16 @@
+import {useContext, useState} from "react";
 export default function LoginPage() {
+    const [username,setUsername] = useState('');
+    const [password,setPassword] = useState('');
+    async function login(ev) {
+        ev.preventDefault();
+        const response = await fetch('http://localhost:4000/login', {
+          method: 'POST',
+          body: JSON.stringify({username, password}),
+          headers: {'Content-Type':'application/json'},
+          credentials: 'include',
+        });
+    }    
     return(
         <form className="login">
             <h1>Login</h1>
